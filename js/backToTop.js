10815eb56fe521682c5db0a069b1d3eda87089fe
Sigal Arthis <arthis.sigal@etudiant.unc.nc>
2024-11-07 19:04:16 +1100

    fix + backToTop + change json content

---
+++ js/backToTop.js
@@ -0,0 +1,13 @@
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+
+window.onscroll = function() {
+    const backToTopButton = document.getElementById("back-to-top");
+    if (document.documentElement.scrollTop > 1000) {
+        backToTopButton.style.display = "flex";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
